6df2d125d4a257a2d316aafb30e028d8f8707ead
Ashok <negiashok132@gmail.com>
2025-01-06 01:37:41 +0530

    int hsi case i create all function and use these all function

--- database.php
+++ database.php
@@ -10,6 +10,7 @@ class Database{
     private $mysqli = "";
     private $result = array();
 
+//-------------------------------------------------------------------------------------------------------->
     public function __construct(){
             if(!$this->conn){
                 $this->mysqli = new mysqli($this->db_host,$this->db_user,$this->db_pass,$this->db_name);
@@ -23,6 +24,7 @@ class Database{
             }
     }
 
+//-------------------------------------------------------------------------------------------------------->
     public function insert($table,$param=array()){
         if($this->tableExists($table)){
             print_r($param);
@@ -45,18 +47,93 @@ class Database{
 
     }
 
-    public function update(){
+//-------------------------------------------------------------------------------------------------------->
+    public function update($table, $param=array(),$where = null){
+        if($this->tableExists($table)){
+            $args = array();
+            foreach ($param as $key => $value) {
+                $args[] = "$key = '$value' ";
+            }
 
+            $sql = "UPDATE $table SET  ". implode(', ',$args);
+            if($where != null){
+                $sql .= " WHERE $where";
+            }
+
+            if($this->mysqli->query($sql)){
+                 array_push($this->result, $this->mysqli->affected_rows);
+                echo "Success Fully Update student data  ";
+                return true;
+            }else{
+                 array_push($this->result, $this->mysqli->error);
+            }
+        }else{
+            return false;
+        }
+     }
+ 
+
+//-------------------------------------------------------------------------------------------------------->
+    public function delete($table , $where=null){
+        if($this->tableExists($table)){
+            $sql = " DELETE FROM $table ";
+            if($where!=null){
+                $sql .= " WHERE $where";
+            }
+            if($this->mysqli->query($sql)){
+                array_push($this->result, $this->mysqli->affected_rows);
+                echo "<br> Successfully deleted ";
+            }else{
+                array_push($this->result, $this->mysqli->error);
+            }
+        }else{
+            return false;
+        }
     }
 
-    public function delete(){
+//-------------------------------------------------------------------------------------------------------->
+    public function select($table,$rows='*',$join=null,$where=null,$order=null,$limit=null){
+            if($this->tableExists($table)){
+                $sql = "SELECT $rows FROM $table";
+                if($join!=null){
+                    $sql .= " JOIN $join";
+                }
+                if($where!=null){
+                    $sql .= " WHERE $where";
+                }
+                if($order!=null){
+                    $sql .= " ORDER BY $order";
+                }
+                if($limit!=null){
+                    $sql .= " LIMIT 0, $limit";
+                }
+                
+                $query = $this->mysqli->query($sql);
+                
+                echo $sql;
+                if($query){
+                    array_push($this->result, $query->fetch_all(MYSQLI_ASSOC));
+                    echo "<br> fetch data through multiple sql parameters";
+                }else{
+                    array_push($this->result,$this->mysqli->error);
+                    echo "<br> error in multiple sql parameters in select function";
+                }
 
+            }
     }
 
-    public function select(){
-
+      public function sql($sql){
+        $query = $this->mysqli->query($sql);
+        if($query){
+            array_push($this->result, $query->fetch_all(MYSQLI_ASSOC));
+            echo "<br> Show data base on simple sql query";
+        }else{
+            array_push($this->result, $query->fetch_all(MYSQLI_ASSOC));
+            echo " <br> failed simple sql query from sql function";
+        }
     }
 
+//-------------------------------------------------------------------------------------------------------->
     private function tableExists($table){
         $sql = "SHOW TABLES FROM $this->db_name LIKE '$table'  ";
         $tableInDb = $this->mysqli->query($sql);
@@ -70,12 +147,14 @@ class Database{
         }
     }
 
+//-------------------------------------------------------------------------------------------------------->
     public function getResult(){
         $val = $this->result;
         $this->result = array();
         return $val;
     }
 
+//-------------------------------------------------------------------------------------------------------->
     public function __destruct(){
         if($this->conn){
             if($this->mysqli->close()){
