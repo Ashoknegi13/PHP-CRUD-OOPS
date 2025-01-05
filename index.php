bd0a7edab0c0e4cc4feea4e418dca3b733f495bf
Ashok <negiashok132@gmail.com>
2025-01-06 01:39:50 +0530

    i use these all function in index file which is created in database file , in this time we can easily perform the crud operation in database

--- index.php
+++ index.php
@@ -2,7 +2,36 @@
 include 'database.php';
 
 $obj = new Database();
-$obj->insert('students',['name'=>'anita' , 'age'=> '21', 'city'=>'rpg']);	
 
- print_r($obj->getResult());
+// $obj->insert('students',['name'=>'anita' , 'age'=> '21', 'city'=>'rpg']);	
+//  print_r($obj->getResult());
+ 
+
+
+// $obj->update('students',['name'=>'anita ashok', 'age'=>'21','city'=>'gwar'], 'id="35" ');
+// print_r($obj->getResult());
+
+
+
+// $obj->update('students',['city'=>'rudraprayag'], 'city="rpg" ');
+//print_r($obj->getResult());
+
+
+// $obj->delete('students','id="44"');
+// echo "<pre>";
+// print_r($obj->getResult());
+// echo "</pre>";
+
+// $obj->sql("SELECT * FROM students WHERE city = 'gurgaon' ");
+//  echo "<pre>";
+//  print_r($obj->getResult());
+//  echo "</pre>";
+
+
+
+// $obj->select('students','*',null,'city="gurgaon"',null,3);
+//  echo "<pre>";
+//  print_r($obj->getResult());
+//  echo "</pre>";
+
 ?>
